9a740819b39b3f7d8b9bd5c2858071f23808bd73
NikolajGrunoski <nikolaj.grunoski@gmail.com>
2020-01-22 12:31:25 +0100

    kk

---
+++ expence-calculator/build/precache-manifest.420f4714f27090faf48da6b7ae1e7977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a078fdcbd39669517e720ed560cf90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24e88844258fb3ec6a8c",
+    "url": "/static/js/2.c8889c86.chunk.js"
+  },
+  {
+    "revision": "be3eb4cbad5a344e9603583a731df32d",
+    "url": "/static/js/2.c8889c86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0270c1e8d193df45767d",
+    "url": "/static/js/main.cae55856.chunk.js"
+  },
+  {
+    "revision": "9873546278e1558c8651",
+    "url": "/static/js/runtime-main.dc7d57ac.js"
+  }
+]);
